0a275773c138d1873377a11b2b1a70d6717c2b69
Nikita Tsukanov <keks9n@gmail.com>
2021-05-14 13:12:27 +0300

    reformat

--- packages/web-app/src/components/ui/Breadcrumbs/breadcrumbs.tsx
+++ packages/web-app/src/components/ui/Breadcrumbs/breadcrumbs.tsx
@@ -2,8 +2,9 @@ import React, { FC } from "react";
 import style from "./breadcrumbs.module.css";
 import { LocaleKeys } from "src/locales/locales";
 import { useLocalizedText } from "src/components/common/LocalizedText";
-import {IntlShape, useIntl} from "react-intl";
-import {hasOwnProperty} from "src/utilities/utils";
+import { IntlShape, useIntl } from "react-intl";
+import { hasOwnProperty } from "src/utilities/utils";
+import Link from "next/link";
 
 export interface LocaleBreadcrumbItem {
   url?: string;
@@ -21,20 +22,17 @@ type BreadcrumbsProps = {
   items: BreadcrumbItem[];
 };
 
-function translate(intl: IntlShape, item: BreadcrumbItem) : {url?: string, title: string} {
+function translate(intl: IntlShape, item: BreadcrumbItem): { url?: string; title: string } {
   let link = "#";
   let title = "";
   if (typeof item === "string") {
     title = item;
   } else {
-    if (item.url != null)
-      link = item.url;
-    if (hasOwnProperty(item, "title"))
-      title = item.title;
-    else
-      title = intl.formatMessage({id: item.localeKey});
+    if (item.url != null) link = item.url;
+    if (hasOwnProperty(item, "title")) title = item.title;
+    else title = intl.formatMessage({ id: item.localeKey });
   }
-  return {url: "/" + intl.locale + link, title: title};
+  return { url: "/" + intl.locale + link, title: title };
 }
 
 export const Breadcrumbs: FC<BreadcrumbsProps> = ({ items }) => {
