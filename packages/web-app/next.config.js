c1a71bd26444b79e05c63227d367dcc6ac88c366
Nikita Tsukanov <keks9n@gmail.com>
2021-05-14 14:26:03 +0300

    Pass through API_BASE_URL env

--- packages/web-app/next.config.js
+++ packages/web-app/next.config.js
@@ -2,4 +2,8 @@ const withPlugins = require("next-compose-plugins");
 const withTM = require("next-transpile-modules")(["@project/components"]);
 const withImages = require("next-images");
 
-module.exports = withPlugins([withTM, withImages]);
+module.exports = withPlugins([withTM, withImages], {
+  serverRuntimeConfig: {
+    API_BASE_URL: process.env.API_BASE_URL
+  },
+});
