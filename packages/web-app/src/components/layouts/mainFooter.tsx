6d07697f25e41a97e6e5973c3e2f50d91466b736
dmitry <stalker2807sky@yandex.ru>
2021-06-18 15:12:39 +0700

    fix

--- packages/web-app/src/components/layouts/mainFooter.tsx
+++ packages/web-app/src/components/layouts/mainFooter.tsx
@@ -71,11 +71,11 @@ export const MainFooter = (props: FooterDataDto) => {
         ))}
         <div className={cn("w-full lg:w-1/3", styles.footer_column, styles.footer_address)}>
           <ul className="flex flex-col font-bold">
-            <li>10 rue de la Lune</li>
-            <li>75002 Paris</li>
+            <li>10 rue de la Lune 75002 Paris</li>
             <li>+33 144 829 031</li>
-            <li>Contact@quartier-latin.com</li>
+            <li>contact@quartier-latin.com</li>
             <li>Skype: QuartierLatinRU</li>
+            <li>WhatsApp: +33 6 26 63 86 27</li>
           </ul>
           {props[lang].writeUs && (
             <ContactUsFormButton className={styles.footer_writeus} footer={true}>
