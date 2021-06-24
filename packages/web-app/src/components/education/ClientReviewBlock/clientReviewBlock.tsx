705361d961f3e265c8069a5fa24a5c66e4e801be
EgorMironov <mironov.e@ad1.ru>
2021-06-24 12:08:04 +0300

    QL-144 fix footer li and hide "All Feedbacks" button

--- packages/web-app/src/components/education/ClientReviewBlock/clientReviewBlock.tsx
+++ packages/web-app/src/components/education/ClientReviewBlock/clientReviewBlock.tsx
@@ -13,10 +13,10 @@ export const ClientReviewBlock: FC<ClientReviewBlockProps> = ({ data }) => {
         <div className={style.clientReviewBlock__container}>
           <div className={style.clientReviewBlock__content}>
             <div className={style.clientReviewBlock__contentText} dangerouslySetInnerHTML={{ __html: data.text }} />
-            <a href={data.button.url} target="_blank" className={style.clientReviewBlock__buttonMore}>
+            {/*<a href={data.button.url} target="_blank" className={style.clientReviewBlock__buttonMore}>
               <img src={icon} className="mdi mdi-arrow-right mr-2" />
               <span className={"pl-2"}>{data.button.title}</span>
-            </a>
+            </a>*/}
           </div>
           <img src={data.img} alt="" className={style.clientReviewBlock__img} />
         </div>
