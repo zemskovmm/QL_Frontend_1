fbd18a9174d7d869be3af358a0e8058586a3e079
dmitry <stalker2807sky@yandex.ru>
2021-09-09 00:23:48 +0700

    check work version

--- packages/web-app/src/pages/_app.tsx
+++ packages/web-app/src/pages/_app.tsx
@@ -9,35 +9,11 @@ import { AppComponentHost } from "src/components/AppComponentHost";
 import App from "next/app";
 import { siteApi } from "../clients/siteApiClient";
 import { GlobalSettingsDto } from "admin-app/src/interfaces/GlobalSettingsDto";
-import { EditorState } from "draft-js";
 
 function MyApp({ Component, pageProps, globalSettings }: AppProps & { globalSettings: GlobalSettingsDto }) {
   const router = useRouter();
   let { urls, title, module } = pageProps;
   if (urls == null) urls = getLanguageUrlsFromRouterState(router);
-
-  return (
-    <>
-      <AppComponentHost
-        requestSetting={globalSettings.requestForm}
-        headTitle={title}
-        headMeta={module?.page.Metadata?.meta}
-      >
-        <MainLayout globalSettings={globalSettings} urls={urls}>
-          <Component {...pageProps} />
-        </MainLayout>
-      </AppComponentHost>
-    </>
-  );
-}
-
-// Technically we are supposed to fetch anything app-wide here,
-// but we need to disable automatic static optimization for
-// server-side locales to work
-MyApp.getInitialProps = async (appContext: AppContext) => {
-  // const appProps = await App.getInitialProps(appContext);
-  // return appProps;
-  const globalSettings = await siteApi.sendRequest(`global/ql/${appContext.router.query["lang"]}`);
   const data = {
     header: {
       headerTopLink: [],
@@ -58,7 +34,29 @@ MyApp.getInitialProps = async (appContext: AppContext) => {
     },
   };
 
-  return { globalSettings: globalSettings ?? data };
+  return (
+    <>
+      <AppComponentHost
+        requestSetting={globalSettings.requestForm ?? data.requestForm}
+        headTitle={title}
+        headMeta={module?.page.Metadata?.meta}
+      >
+        <MainLayout globalSettings={globalSettings ?? data} urls={urls}>
+          <Component {...pageProps} />
+        </MainLayout>
+      </AppComponentHost>
+    </>
+  );
+}
+
+// Technically we are supposed to fetch anything app-wide here,
+// but we need to disable automatic static optimization for
+// server-side locales to work
+MyApp.getInitialProps = async (appContext: AppContext) => {
+  const appProps = await App.getInitialProps(appContext);
+  // const globalSettings = await siteApi.sendRequest(`https://ql.dotlic.ru/api/global/ql/en`);
+
+  return appProps;
 };
 
 export default MyApp;
