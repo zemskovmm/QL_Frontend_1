7b2f61dcebeb2ec87946a3382d7c343b9e84e524
dmitry <stalker2807sky@yandex.ru>
2021-09-30 11:21:46 +0700

    save

--- packages/admin-app/src/pages/globalSetting/page.tsx
+++ packages/admin-app/src/pages/globalSetting/page.tsx
@@ -7,6 +7,7 @@ import { RequestFormSettings } from "./requestFormSetting/requestFormSetting";
 import { HeaderSettings } from "./headerSetting/headerSetting";
 import { FooterSettings } from "./footerSetting/footerSetting";
 import { ChooseLang } from "./common/commonGlobal";
+import { PersonalAreaSettings } from "./personalAreaSetting/personalAreaSetting";
 
 export const AdminGlobalSettingEditor = () => {
   const { globalSettingsPage: s } = useRootStore();
@@ -15,14 +16,17 @@ export const AdminGlobalSettingEditor = () => {
   return useObserver(() => (
     <div className={`max-w-7xl py-10 mx-auto px-10`}>
       <ChooseLang />
-      <DropDownLista name={`Header`} active={name === "Header"} onClick={() => setName(`Header`)}>
+      <DropDownList name={`Header`} active={name === "Header"} onClick={() => setName(`Header`)}>
         <HeaderSettings s={s} />
-      </DropDownLista>
-      <DropDownList name={`Request Form`} active={name === "Request Form"} onClick={() => setName(`Request Form`)}>
-        <RequestFormSettings s={s} />
       </DropDownList>
       <DropDownList name={`Footer`} active={name === "Footer"} onClick={() => setName(`Footer`)}>
         <FooterSettings s={s} />
+      </DropDownList>
+      <DropDownList name={`Request Form`} active={name === "Request Form"} onClick={() => setName(`Request Form`)}>
+        <RequestFormSettings s={s} />
+      </DropDownList>
+      <DropDownList name={`Personal Area`} active={name === "Personal Area"} onClick={() => setName(`Personal Area`)}>
+        <PersonalAreaSettings s={s} />
       </DropDownList>
       <AdminButton color={`save`} onClick={() => s.save()}>
         Save
--- packages/admin-app/src/pages/globalSetting/personalAreaSetting/personalAreaSetting.tsx
+++ packages/admin-app/src/pages/globalSetting/personalAreaSetting/personalAreaSetting.tsx
@@ -1,8 +1,16 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { useObserver } from "mobx-react";
 import { GlobalSettingsPageStore } from "../../../stores/pages/globalSettings/globalSettingsPageStore";
 import { GlobalLink, GlobalSocialLink } from "../common/commonGlobal";
+import { DropDownList } from "@project/components/src/blocks/FaqBlock/faqBlock";
 
-export const PersonalAreaSetting: FC<{ s: GlobalSettingsPageStore }> = ({ s }) => {
-  return useObserver(() => <div></div>);
+export const PersonalAreaSettings: FC<{ s: GlobalSettingsPageStore }> = ({ s }) => {
+  const [name, setName] = useState("");
+  return useObserver(() => (
+    <div>
+      <DropDownList name={`Profile`} active={name === "Profile"} onClick={() => setName(`Profile`)}>
+        <div />
+      </DropDownList>
+    </div>
+  ));
 };
