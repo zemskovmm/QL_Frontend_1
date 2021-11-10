98cdbc8be61000425e0d68a191878efb31c0cb17
dmitry <stalker2807sky@yandex.ru>
2021-11-11 05:00:07 +0700

    merge

--- packages/user-dashboard/src/components/Header.tsx
+++ packages/user-dashboard/src/components/Header.tsx
@@ -4,17 +4,15 @@ import { Link, route } from "preact-router";
 import { useRouterStore } from "stores/RouterStore";
 import { Button } from "@project/components/src/ui-kit/Button";
 import { useUserStatuseStore } from "stores/UserStatuseStore";
-import BURGER_ICON from "@project/components/src/assets/icons/burger.svg";
 import USER_ICON from "@project/components/src/assets/icons/user.svg";
 import { Icon } from "@project/components/src/ui-kit/Icon";
 import { Logo } from "@project/components/src/ui-kit/Logo";
 import { LangChooser } from "@project/components/src/ui-kit/LangChooser";
 import { useLocalesStore } from "stores/LocalesStore";
-import { Dropmenu } from "@project/components/src/ui-kit/Dropmenu";
-import { LeftNavigation } from "./LeftNavigation";
+import { MenuBurger } from "components/MenuBurger";
 
 export const Header: FunctionalComponent = () => {
-  const { isLogined, isUnlogined, user } = useUserStatuseStore();
+  const { isLogined, user } = useUserStatuseStore();
   const { langUrls, HOME_PATH, PROFILE_PATH, SIGN_IN_PATH } = useRouterStore();
   const { lang } = useLocalesStore();
 
@@ -34,7 +32,7 @@ export const Header: FunctionalComponent = () => {
             urls={langUrls}
             linkComponent={({ url, lang }) => (
               <Link
-                onClick={(e) => {
+                onClick={() => {
                   route(url);
                 }}
                 href={url}
@@ -49,13 +47,7 @@ export const Header: FunctionalComponent = () => {
               <Link href={PROFILE_PATH}>
                 <Icon src={USER_ICON} alt={profileName} size="8" />
               </Link>
-              <Dropmenu
-                className="inline tablet:hidden"
-                content={<Icon src={BURGER_ICON} alt="menu" size="8" />}
-                position="rightScreen"
-              >
-                <LeftNavigation />
-              </Dropmenu>
+              <MenuBurger className="inline tablet:hidden" />
             </>
           ) : (
             <Link href={SIGN_IN_PATH}>
