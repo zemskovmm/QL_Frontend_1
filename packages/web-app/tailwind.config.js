7cd1312c92b4581dd88badf337361bbbc0746de9
dmitry <stalker2807sky@yandex.ru>
2021-11-16 04:31:40 +0700

    fix title tab block

--- packages/web-app/tailwind.config.js
+++ packages/web-app/tailwind.config.js
@@ -1,29 +1,30 @@
 module.exports = {
   purge: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    '../components/src/**/*.{js,ts,jsx,tsx}'
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "../components/src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     colors: {
-      primary: 'var(--color-primary)',
-      secondary: 'var(--color-secondary)',
-      third: 'var(--color-third)',
-      bgprimary: 'var(--color-bg-primary)',
-      bgsecondary: 'var(--color-bg-secondary)',
-      bdprimary: 'var(--color-bd-primary)',
-      hover: 'var(--color-hover)'
+      primary: "var(--color-primary)",
+      secondary: "var(--color-secondary)",
+      third: "var(--color-third)",
+      bgprimary: "var(--color-bg-primary)",
+      bgsecondary: "var(--color-bg-secondary)",
+      bdprimary: "var(--color-bd-primary)",
+      hover: "var(--color-hover)",
+      white: "white",
     },
     boxShadow: {
-      DEFAULT: '0 5px 10px rgba(0, 78, 255, 0.05), 0 10px 30px rgba(0, 78, 255, 0.03);',
+      DEFAULT: "0 5px 10px rgba(0, 78, 255, 0.05), 0 10px 30px rgba(0, 78, 255, 0.03);",
     },
     extend: {
       spacing: {
-        128: '32rem',
+        128: "32rem",
       },
       borderRadius: {
-        primary: '0.1875rem'
-      }
+        primary: "0.1875rem",
+      },
     },
   },
   variants: {
@@ -34,4 +35,4 @@ module.exports = {
                 containerMaxWidths: { sm: '540px', md: '720px', lg: '960px', xl: '1140px' },
             }),*/
   ],
-}
+};
