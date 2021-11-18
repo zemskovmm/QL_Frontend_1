06c46fd82d3f7a65ea379b397ecdc5d280e79522
dmitry <stalker2807sky@yandex.ru>
2021-11-18 12:54:05 +0700

    merge

--- packages/user-dashboard/src/components/Header.tsx
+++ packages/user-dashboard/src/components/Header.tsx
@@ -2,7 +2,6 @@ import { FunctionalComponent } from "preact";
 import { Container } from "components/Container";
 import { Link, route } from "preact-router";
 import { useRouterStore } from "stores/RouterStore";
-import { Button } from "@project/components/src/ui-kit/Button";
 import { useUserStatuseStore } from "stores/UserStatuseStore";
 import USER_ICON from "@project/components/src/assets/icons/user.svg";
 import { Icon } from "@project/components/src/ui-kit/Icon";
@@ -13,7 +12,7 @@ import { MenuBurger } from "components/MenuBurger";
 
 export const Header: FunctionalComponent = () => {
   const { isLogined, user } = useUserStatuseStore();
-  const { langUrls, PROFILE_PATH, SIGN_IN_PATH } = useRouterStore();
+  const { langUrls, PROFILE_PATH } = useRouterStore();
   const { lang } = useLocalesStore();
 
   const profileName =
@@ -22,7 +21,7 @@ export const Header: FunctionalComponent = () => {
   return (
     <div className="mb-10 md:mb-0 px-3 md:px-10 py-2.5 md:py-1 box-content shadow h-20 lg:absolute lg:left-0 lg:right-0 lg:top-0 lg:z-50">
       <Container className="flex justify-between items-center h-full">
-          <Logo />
+        <Logo />
         <div className="relative inline-flex gap-5 items-center">
           <LangChooser
             lang={lang}
@@ -39,12 +38,14 @@ export const Header: FunctionalComponent = () => {
             )}
           />
 
-          {isLogined && <>
+          {isLogined && (
+            <>
               <Link href={PROFILE_PATH}>
                 <Icon src={USER_ICON} alt={profileName} size="8" />
               </Link>
               <MenuBurger className="inline tablet:hidden" />
-          </>}
+            </>
+          )}
         </div>
       </Container>
     </div>
