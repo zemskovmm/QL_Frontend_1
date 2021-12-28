5a5bbe8ba53fde51eb326497a4f66ed6c595b0d8
dmitry <stalker2807sky@yandex.ru>
2021-12-28 23:30:55 +0700

    add flag

--- packages/web-app/src/assets/icons/flags/index.tsx
+++ packages/web-app/src/assets/icons/flags/index.tsx
@@ -1,25 +1,30 @@
-import En from "src/assets/images/flags/en.svg"
-import Ru from "src/assets/images/flags/ru.svg"
-import Fr from "src/assets/images/flags/fr.svg"
-import Esp from "src/assets/images/flags/esp.svg"
-import Cn from "src/assets/images/flags/cn.svg"
+import En from "src/assets/images/flags/en.svg";
+import Ru from "src/assets/images/flags/ru.svg";
+import Fr from "src/assets/images/flags/fr.svg";
+import Esp from "src/assets/images/flags/esp.svg";
+import Cn from "src/assets/images/flags/cn.svg";
+import EnFr from "src/assets/images/flags/enfr.svg";
 
 export interface socialProps {
-  icon: string
+  icon: string;
 }
 
-export default ({icon}:socialProps) => {
-   switch (icon) {
-    case 'fr':
-      return <img src={Fr} alt={icon} />
-    case 'ru':
-      return <img src={Ru} alt={icon} />
-    case 'esp':
-       return <img src={Esp} alt={icon} />
-     case 'cn':
-       return <img src={Cn} alt={icon} />
-    case 'en':
+const Flags = ({ icon }: socialProps) => {
+  switch (icon) {
+    case "fr/en":
+      return <img src={EnFr} alt={icon} />;
+    case "fr":
+      return <img src={Fr} alt={icon} />;
+    case "ru":
+      return <img src={Ru} alt={icon} />;
+    case "esp":
+      return <img src={Esp} alt={icon} />;
+    case "cn":
+      return <img src={Cn} alt={icon} />;
+    case "en":
     default:
-      return <img src={En} alt='en' />
+      return <img src={En} alt="en" />;
   }
-}
+};
+
+export default Flags;
